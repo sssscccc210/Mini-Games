e0e269763a6a2aecdde82e75f743207598f2b3f3
ShuntaKasuya <sssscccc210@users.noreply.github.com>
2025-09-09 00:05:15 +0900

    localize Random game into multiple languages

--- src/lib/games/RandomGame.svelte
+++ src/lib/games/RandomGame.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { t } from '../../translations/translations';
+
   let count = 0;
   let clear = false;
 
@@ -17,7 +19,7 @@
 <div class="bg-violet-200 w-3xl max-w-full h-fit min-h-50 max-h-full items-center text-center content-center p-6">
   {#if clear}
     <h1 class="text-3xl sm:text-4xl font-bold mb-4 text-green-600">🎉<br>Congratulations!<br>You Win!</h1>
-    <p class="text-lg mb-4">You clicked the button {count} times.</p>
+    <p class="text-lg mb-4">{$t('common.you_clicked_the_button')}{count} {$t('common.count_times')}</p>
   {:else}
     <h1 class="text-3xl mb-4">CLICK!</h1>
     <button onclick={increment} class="w-fit h-fit text-xl font-bold text-white bg-pink-400 rounded-lg px-4 py-1 hover:bg-pink-500 duration-200">
